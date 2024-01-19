040729fdcfca25cc1792de315d83c5b375644446
Temirkozha <Zhanarys>
2024-01-19 14:45:47 +0600

    added changes

--- lab8.cpp
+++ lab8.cpp
@@ -8,7 +8,6 @@ int main(){
         int num;
         cin>>num;
         v.push_back(num);
-
     }
     sort(v.begin(),v.end());
     for(int i=0;i<n;i++){
--- test.cpp
+++ test.cpp
@@ -2,15 +2,12 @@
     #include<vector>
     #include<algorithm>
     using namespace std;
-
     int main() {
         int n;
         cin >> n;
-
         vector<int> a(n);
         vector<int> b;
         vector<int> c;
-
         for (int i = 0; i < n; i++) {
             cin >> a[i]; 
         }
@@ -27,10 +24,8 @@
         for (int i = 0; i < c.size(); i++) {
             cout << c[i] << " ";
         }
-
         for (int i = 0; i < b.size(); i++) {
             cout << b[i] << " ";
         }
-
         return 0;
     }
